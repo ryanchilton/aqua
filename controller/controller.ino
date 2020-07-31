5e9219527336a35482b9e138c0c748fb2037d74a
ryan <ryankchilton@gmail.com>
2020-07-31 13:24:58 -0400

    * updates after testing on July 30

--- controller/controller.ino
+++ controller/controller.ino
@@ -19,9 +19,11 @@
 // 86,400,000 ms in a day
 #define FEEDER_PERIOD_MS    10000
 #define FEEDER_DURATION_MS  1000
-#define PUMP_1_PERIOD_MS    20000
-#define PUMP_1_DURATION_MS  10000
+#define PUMP_1_PERIOD_MS    180000
+#define PUMP_1_DURATION_MS  120000
 #define PUMP_2_DURATION_MS  10000
+
+bool e_stop = false; // emergency stop
 
 
 // initialize serial interface for temperature sensors
@@ -30,7 +32,7 @@ OneWire oneWire_2(TEMP_2_PIN);
 DS18B20 temp_sens_1(&oneWire_1);
 DS18B20 temp_sens_2(&oneWire_2);
 
-// set up timers for time-triggered events
+// set up timers for time-triggered events and event-triggered events
 TimeTrigger feeder_timer(FEEDER_PERIOD_MS, FEEDER_DURATION_MS);
 TimeTrigger pump_1_timer(PUMP_1_PERIOD_MS, PUMP_1_DURATION_MS);
 Timer       pump_2_timer(PUMP_2_DURATION_MS);
@@ -47,14 +49,6 @@ void setup() {
 
   // Connect to Arduino IoT Cloud
   ArduinoCloud.begin(ArduinoIoTPreferredConnection);
-  
-  /*
-     The following function allows you to obtain more information
-     related to the state of network and IoT Cloud connection and errors
-     the higher number the more granular information you’ll get.
-     The default is 0 (only errors).
-     Maximum is 4
- */
   setDebugMessageLevel(2);
   ArduinoCloud.printDebugInfo();
 
@@ -74,18 +68,8 @@ void setup() {
   
 }
 
-// main loop
-void loop() {
-  // update cloud interface
-  ArduinoCloud.update();
-
-  // read water sensors
-  int water_sensor_1_voltage = analogRead(WATER_SENSOR_1_PIN);
-  water_sensor_1 = water_sensor_1_voltage * 100.0 / 1024.0;
-
-  int water_sensor_2_voltage = analogRead(WATER_SENSOR_2_PIN);
-  float water_sensor_2 = water_sensor_2_voltage * 100.0 / 1024.0;
-
+bool readTemps()
+{
   // read tempurature sensors
   temp_sens_1.requestTemperatures();
   while (!temp_sens_1.isConversionComplete());  // wait until sensor is ready
@@ -95,16 +79,65 @@ void loop() {
   while (!temp_sens_2.isConversionComplete());  // wait until sensor is ready
   temp_2 = temp_sens_2.getTempC();
 
-  Serial.println("water sensor 1: " + String(water_sensor_1));
-  Serial.println("water sensor 2: " + String(water_sensor_2));
-  if(water_sensor_2 > 40)
+  Serial.println("temp 1: " + String(temp_1));
+  Serial.println("temp 2: " + String(temp_2));
+}
+
+bool growBedWaterLevelTooHigh()
+{
+  // read water sensors
+  int water_sensor_1_voltage = analogRead(WATER_SENSOR_1_PIN);
+  water_sensor_1 = water_sensor_1_voltage * 100.0 / 1024.0;
+
+  Serial.println("Grow bed water sensor: " + String(water_sensor_1));
+
+  return water_sensor_1 > 45;
+}
+
+bool sumpBucketWaterLevelTooHigh()
+{
+  int water_sensor_2_voltage = analogRead(WATER_SENSOR_2_PIN);
+  float water_sensor_2 = water_sensor_2_voltage * 100.0 / 1024.0;
+
+  Serial.println("Sump bucket water sensor: " + String(water_sensor_2));
+
+  return water_sensor_2 > 45;
+}
+
+// main loop
+void loop() {
+  // update cloud interface
+  //ArduinoCloud.update();
+
+  if (growBedWaterLevelTooHigh())
   {
-    pump_2_timer.start();
+    e_stop = true;
   }
-  
-  digitalWrite(FEEDER_RELAY_PIN, feeder_timer.isActive());
-  digitalWrite(PUMP_1_RELAY_PIN, pump_1_timer.isActive());
+
+  if(sumpBucketWaterLevelTooHigh()) // if sump bucket water level is high
+  {
+    pump_2_timer.start(); // start the timer that will start the sump pump
+  }
+
+  // feeder motor control
+  //digitalWrite(FEEDER_RELAY_PIN, feeder_timer.isActive());
+
+  // sump pump control
   digitalWrite(PUMP_2_RELAY_PIN, pump_2_timer.isActive());
+
+  // tank pump control
+  if (e_stop == true) // water level problem
+  {
+    Serial.println("emergency stop");
+    digitalWrite(PUMP_1_RELAY_PIN, 0); // turn of main pump
+  }
+  else
+  {
+    digitalWrite(PUMP_1_RELAY_PIN, pump_1_timer.isActive()); // turn main pump on or off based on timer
+  }
+
+  // read and print temperatures
+  readTemps();
   
   delay(10);
 }
